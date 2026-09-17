b8e74aa195a3fa175b9e1794cb3992b645094784
Selbarins <selbarins@gmail.com>
2026-09-17 08:29:35 +0100

    Add config page skeleton

---
+++ src/app/config/page.tsx
@@ -0,0 +1,8 @@
+export default function ConfigPage() {
+  return (
+    <main className="min-h-screen p-4">
+      <h1 className="text-xl font-semibold">Player Config</h1>
+      {/* TODO: Wealth / Vitality / Focus / Momentum / Season / Quest / Boss toggles */}
+    </main>
+  );
+}
