01c6005181f0097ab9b457dbb261fc433a05c94a
Selbarins <selbarins@gmail.com>
2026-09-17 08:30:35 +0100

    Add core tables migration skeleton (empty statements)

---
+++ supabase/migrations/00001_core_tables.sql
@@ -0,0 +1,29 @@
+-- Pulse core schema skeleton
+-- Split further as tables grow (e.g. 00002_config.sql, 00003_roa.sql)
+
+-- player_profile
+-- CREATE TABLE player_profile (...);
+
+-- attributes
+-- CREATE TABLE attributes (...);
+
+-- daily_logs
+-- CREATE TABLE daily_logs (...);
+
+-- training_sessions
+-- CREATE TABLE training_sessions (...);
+
+-- financial_logs
+-- CREATE TABLE financial_logs (...);
+
+-- quest_completions
+-- CREATE TABLE quest_completions (...);
+
+-- boss_results
+-- CREATE TABLE boss_results (...);
+
+-- edge_events
+-- CREATE TABLE edge_events (...);
+
+-- roa_entries
+-- CREATE TABLE roa_entries (...);
