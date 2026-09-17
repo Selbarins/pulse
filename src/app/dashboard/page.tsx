a68925eacf48c9a3b31a13cc736b77f6a1aed31c
Selbarins <selbarins@gmail.com>
2026-09-17 08:29:33 +0100

    Add dashboard page skeleton

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <main className="min-h-screen p-4">
+      <h1 className="text-xl font-semibold">Dashboard</h1>
+      {/* TODO: Attribute bars, quests, bosses, edge, money/health status */}
+    </main>
+  );
+}
