9b29fdfd63e13862b1dbe96d8e7b13d9137425b3
Selbarins <selbarins@gmail.com>
2026-09-17 08:30:39 +0100

    Add config + pivot tables migration skeleton

---
+++ supabase/migrations/00002_config_and_pivots.sql
@@ -0,0 +1,15 @@
+-- Config & Pivot tables (from Project Memory)
+
+-- player_config
+-- CREATE TABLE player_config (
+--   id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+--   domain text NOT NULL,
+--   config_json jsonb NOT NULL,
+--   updated_at timestamptz DEFAULT now()
+-- );
+
+-- config_change_log
+-- CREATE TABLE config_change_log (...);
+
+-- pivot_events
+-- CREATE TABLE pivot_events (...);
